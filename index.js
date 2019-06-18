3b6814bc0e490c6881360c4758b94e7989c81631
Prajvi <prajvi.bakshi@gmail.com>
2019-06-18 17:00:12 -0400

    JS code examples day 2

--- index.js
+++ index.js
@@ -60,3 +60,103 @@ if (johnAvg > mikeAvg && johnAvg > maryAvg) {
 } else {
   console.log('There was an error in processing your request');
 }
+
+let tipArray = [];
+let totalBillArray = [];
+
+function calculateTip(bill) {
+  let tip = 0;
+  let totalAmount = 0;
+  if (bill < 50) {
+    tip = 0.20 * bill;
+    totalAmount = tip + bill;
+    arrayPush(tip, totalAmount);
+  } else if (bill >= 50 && bill <= 200) {
+    tip = 0.15 * bill;
+    totalAmount = tip + bill;
+    arrayPush(tip, totalAmount);
+  } else {
+    tip = 0.10 * bill;
+    totalAmount = tip + bill;
+    arrayPush(tip, totalAmount);
+  }
+}
+
+function arrayPush(tipAmt, billAmount) {
+  tipArray.push(tipAmt);
+  totalBillArray.push(billAmount);
+}
+
+function printArrays() {
+  console.log('Initial Bills were: ' + initialBills);
+  console.log('Tip Amounts were: ' + tipArray);
+  console.log('Total Bills were: ' + totalBillArray);
+}
+
+let initialBills = [124, 48, 268];
+
+calculateTip(124);
+calculateTip(48);
+calculateTip(268);
+printArrays();
+
+//Objects are basically key:value pairs. Can contain arrays, methods and other objects too
+//Object literal
+let john = {
+  firstName: 'John',
+  lastName: 'Smith',
+  birthYear: 1992,
+  family: ['Jane', 'Jack', 'Mary', 'Bob'],
+  job: 'teacher',
+  isMarried: false,
+  calcAge: function() {
+    //Below is the same as saying john.age = 2018 - john.birthYear. 'this' points to the object itself
+    //Not only can 'this' get a value, but it can 'set' and define a value (seen below)
+    this.age = 2018 - this.birthYear;
+  }
+};
+
+
+console.log(john['lastName']);
+console.log(john.lastName);
+console.log(john.family);
+console.log(john.calcAge(1990));
+john.isMarried = true;
+console.log(john);
+
+let jim = new Object();
+jim.firstName = 'Jim';
+jim.birthYear = 1965;
+jim['lastName'] = 'Jam';
+
+
+/***************************
+* CODING CHALLENGE 4
+*/
+
+let mark = {
+  fullName: 'Mark Jones',
+  mass: 75,
+  height: 1.65,
+  calcBMI: function() {
+    //set new variable BMI //get variables mass and height
+    this.bmi = this.mass/Math.pow((this.height),2);
+    return this.bmi;
+  }
+};
+
+let tim = {
+  fullName: 'Tim Smith',
+  mass: 72,
+  height: 1.75,
+  calcBMI: function() {
+    this.bmi = this.mass/Math.pow((this.height),2);
+    return this.bmi;
+  }
+};
+
+if (mark.calcBMI() > tim.calcBMI()) {
+  console.log(mark.fullName + ' has a higher BMI of ' + mark.bmi);
+} else if (tim.calcBMI() > mark.calcBMI()) {
+  console.log(tim.fullName + ' has a higher BMI of ' + tim.bmi);
+}
